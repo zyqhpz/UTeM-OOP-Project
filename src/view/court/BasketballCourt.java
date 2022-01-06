59f1a99b20329adf1678e65aa46c9a848d10efa5
Carol0803 <carolwong.0803@gmail.com>
2022-01-06 13:15:21 +0800

    connect court's button to database

--- src/view/court/BasketballCourt.java
+++ src/view/court/BasketballCourt.java
@@ -7,6 +7,7 @@ import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.SQLException;
 
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
@@ -19,13 +20,17 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+
+import controller.CourtController;
+import model.Court;
+
 import javax.swing.JTextField;
 
 public class BasketballCourt extends JFrame {
 
 	private JPanel contentPane;
 	private Color colour = new Color(62, 180, 137);
-	private JTextField txtBack;
+	private Color highlight = new Color(222, 244, 64);
 
 	/**
 	 * Launch the application.
@@ -45,8 +50,10 @@ public class BasketballCourt extends JFrame {
 
 	/**
 	 * Create the frame.
+	 * @throws SQLException 
+	 * @throws ClassNotFoundException 
 	 */
-	public BasketballCourt() {
+	public BasketballCourt() throws ClassNotFoundException, SQLException {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setSize(900, 800);
 		setTitle("Basketball Court");
@@ -57,22 +64,84 @@ public class BasketballCourt extends JFrame {
 		JButton btnB1 = new JButton("B1");
 		btnB1.setBounds(80, 80, 343, 603);
 		btnB1.setFont(new Font("Times New Roman", Font.PLAIN, 25));
-		btnB1.setBackground(colour);
-		btnB1.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-					btnB1.setBackground(Color.RED);
-			}
-		});
+		CourtController ccb1 = new CourtController();
+		int status = -1;
+		status = ccb1.checkStatus("B1");
+		if(status == 1) {
+			btnB1.setBackground(Color.RED);
+			btnB1.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+						btnB1.setBackground(colour);
+						try {
+							ccb1.setStatus("0", "B1");
+						} catch (ClassNotFoundException e1) {
+							// TODO Auto-generated catch block
+							e1.printStackTrace();
+						} catch (SQLException e1) {
+							// TODO Auto-generated catch block
+							e1.printStackTrace();
+						}
+					}
+				});
+		}
+		else if(status == 0){
+			btnB1.setBackground(colour);
+			btnB1.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+						btnB1.setBackground(highlight);
+						try {
+							ccb1.setStatus("1", "B1");
+						} catch (ClassNotFoundException e1) {
+							// TODO Auto-generated catch block
+							e1.printStackTrace();
+						} catch (SQLException e1) {
+							// TODO Auto-generated catch block
+							e1.printStackTrace();
+						}
+					}
+				});
+		}
 		
 		JButton btnB2 = new JButton("B2");
 		btnB2.setBounds(463, 80, 343, 603);
 		btnB2.setFont(new Font("Times New Roman", Font.PLAIN, 25));
-		btnB2.setBackground(colour);
-		btnB2.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-					btnB2.setBackground(Color.RED);
-			}
-		});
+		CourtController ccb2 = new CourtController();
+		int status2 = -1;
+		status2 = ccb2.checkStatus("B2");
+		if(status2 == 1) {
+			btnB2.setBackground(Color.RED);
+			btnB2.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+						btnB2.setBackground(colour);
+						try {
+							ccb2.setStatus("0", "B2");
+						} catch (ClassNotFoundException e1) {
+							// TODO Auto-generated catch block
+							e1.printStackTrace();
+						} catch (SQLException e1) {
+							// TODO Auto-generated catch block
+							e1.printStackTrace();
+						}
+					}
+				});
+		}
+		else if(status2 == 0){
+			btnB2.setBackground(colour);
+			btnB2.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+						btnB2.setBackground(highlight);
+						try {
+							ccb2.setStatus("1", "B2");
+						} catch (ClassNotFoundException e1) {
+							// TODO Auto-generated catch block
+							e1.printStackTrace();
+						} catch (SQLException e1) {
+							// TODO Auto-generated catch block
+							e1.printStackTrace();
+						}
+					}
+				});
+		}
 		
 		contentPane.setLayout(null);
 		contentPane.add(btnB1);
